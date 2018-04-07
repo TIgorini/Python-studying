153c72a7d97384013e1bf7de799df3aad9b16543
tigorini <mr7igrik@gmail.com>
2018-04-08 02:14:24 +0300

    demo version of parsing (find_emails() func)

--- find_emails.py
+++ find_emails.py
@@ -1,8 +1,24 @@
 import requests
+import re
 from bs4 import BeautifulSoup
+
+
+def find_emails(url, depth):
+    if depth < 3:
+        r = requests.get(url.text)
+        soup = BeautifulSoup(r.text, 'lxml')
+        email_pattern = re.compile('[a-zA-Z0-9_]+@[a-zA-Z0-9_]+\.com')
+        for email in email_pattern.findall(soup.get_text()):
+            add_email(email)
+
+def add_email(email):
+    if email not in emails:
+        emails.append(email)
+
 
 with open('urls.xml', 'r') as xml_file:
     soup = BeautifulSoup(xml_file, 'xml')
 
+emails = []
 for url in soup.find_all('url'):
-    r = requests.get(url.text)
+   find_emails(url, 0)
